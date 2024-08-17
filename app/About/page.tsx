40ecbe900957961aa8580ccabcd9676a2f59da0b
JosiahRailton <jrailton222@gmail.com>
2024-08-17 11:41:26 -0400

    Simple Nav Links

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -3,16 +3,16 @@ import Layout from './layout';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-export default function Home() {
+export default function About() {
   return (
     <Layout>
       <header>
         <h1>Josiah Railton</h1>
         <nav>
           <div className="navLinks">
-            <a href="/">Home</a>
-            <a href="/About">About</a>
-            <a href="/Contact">Contact</a>
+          <a href="../">Home</a>
+            <a href="../About">About</a>
+            <a href="../Contact">Contact</a>
           </div>
           <div className="socialIcons">
             <a href="https://www.linkedin.com/in/josiah-railton-jrailton222" target="_blank" rel="noopener noreferrer"><LinkedInIcon></LinkedInIcon></a>
@@ -20,7 +20,7 @@ export default function Home() {
           </div>
         </nav>
       </header>
-      <h2>Home</h2>
+      <h2>About</h2>
     </Layout>
   );
 }
