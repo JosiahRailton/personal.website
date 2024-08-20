90e7f56494d48dcb72b10da0b013c6c6ef77c21d
JosiahRailton <jrailton222@gmail.com>
2024-08-19 22:05:19 -0400

    Tech Skills Section Start

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,25 @@ export default function Home() {
         </div>
       </div>
       <div>
+        <div id="skills">
+          <div id="skillsDescription">
+            <h2> Main Tech Skills</h2>
+            <p> These are some main areas I have experience in</p>
+          </div>
+          <div id="skillsDisplay-flex">
+            <div> TypeScript </div>
+            <div> JavaScript </div>
+            <div> Next.js </div>
+            <div> React </div>
+            <div> HTML </div>
+            <div> CSS </div>
+            <div> MySQL </div>
+            <div> Database Design </div>
+            <div> SQL </div>
+            <div> Python </div>
+          </div>
+          <p> </p>
+        </div>
         <p>More info down here</p>
       </div>
     </div>
